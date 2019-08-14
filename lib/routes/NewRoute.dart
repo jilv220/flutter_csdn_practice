64a38222d455d87341e718fa1680e8543786dd47
jm <lv.ji@jiamaintech.com>
2019-08-14 18:31:10 +0800

    refactor the code

--- lib/routes/NewRoute.dart
+++ lib/routes/NewRoute.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class NewRoute extends StatelessWidget {
+class NewRoute extends StatefulWidget {
+  @override
+  _NewRouteState createState() => _NewRouteState();
+}
+
+
+class _NewRouteState extends State<NewRoute> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+  List tabs = ["新闻","娱乐","历史"];
+
   @override
   Widget build(BuildContext context) {
+
+    _tabController = TabController(length: tabs.length,vsync: this);
+
     return Scaffold(
       appBar: AppBar(
         title: Text ("new route"),
+        bottom: TabBar(
+          tabs: tabs.map((element) => Tab(text: element,)).toList(),
+          controller: _tabController,
+        ),
       ),
-      body: Center(
-        child: Text("This is a new route"),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Text("haha"),
+          Text("haha"),
+          Text("haha"),
+        ],
       ),
     );
+
   }
 }
