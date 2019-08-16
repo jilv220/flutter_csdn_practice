ae44c868adb7d7b49ebf83939ac1a4b76e507d56
jilv220 <lyuji220@hotmail.com>
2019-08-16 20:54:20 +0800

    refactor homepage code

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter_app/routes/TipRoute.dart';
 
 //main 之前可以自定义异常捕获
 void main() => runApp(MyApp());
-String text = "我是参数";
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -39,7 +38,30 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   int _currentIndex = 0;
-  final List<Widget> bottomBarViews = [
+
+  final List<BottomNavigationBarItem> bottomNavItems = [
+    new BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/Icons/MainPage.png")),
+      title: Text("首页"),
+    ),
+    new BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/Icons/Study.png")),
+      title: Text("学习"),
+    ),
+    new BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/Icons/Blink.png")),
+      title: Text("Blink"),
+    ),
+    new BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/Icons/Forum.png")),
+      title: Text("论坛"),
+    ),
+    new BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        title: Text("我的"),
+    )];
+
+  final List<Widget> pages = [
     MainRoute(),
     TipRoute(),
     AlignRoute(),
@@ -52,30 +74,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: null,
-      body: bottomBarViews[_currentIndex],
+      body: pages[_currentIndex],
       bottomNavigationBar: new BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            new BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage("assets/Icons/MainPage.png")),
-                title: Text("首页"),
-            ),
-            new BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("assets/Icons/Study.png")),
-              title: Text("学习"),
-            ),
-            new BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("assets/Icons/Blink.png")),
-              title: Text("Blink"),
-            ),
-            new BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("assets/Icons/Forum.png")),
-              title: Text("论坛"),
-            ),
-            new BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text("我的"),
-            ),
-          ],
+        items:bottomNavItems,
         type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.red,
         currentIndex: _currentIndex,
--- lib/net/NetRequestEnum.dart
+++
@@ -1,5 +0,0 @@
-
-enum NetRequest {
-  GET,
-  POST
-}
